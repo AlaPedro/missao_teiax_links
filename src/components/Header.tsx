3f62b91de95f0961a408ced1f14619daa21a3e37
Alã Pedro <alanpedro94@gmail.com>
2024-12-03 17:28:49 -0300

    feet: adiciona router do icon do header para dashboard

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 export default function Header() {
+    const router = useRouter()
     return (
         <header className="absolute top-0 w-full h-[74px] bg-primaryBlue flex justify-between">
             <div>
                 <Image
-                    className="relative top-[18px] left-[30px]"
+                    className="relative top-[18px] left-[30px] cursor-pointer"
                     src={'/logo-1.svg'}
                     width={40}
                     height={40}
                     alt="Logo"
+                    onClick={() => {
+                        router.push('/dashboard')
+                    }}
                 />
             </div>
             <div className="flex items-center gap-4 mr-[30px] cursor-pointer">
