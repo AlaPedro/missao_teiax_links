abe6e29cac7f2158e0d0850729cc28107ff2d156
Alã Pedro <alanpedro94@gmail.com>
2024-12-03 20:00:19 -0300

    fix: correção de grafia da index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="bg-primaryBlue w-screen h-screen text-primaryWhite flex flex-col justify-center items-center gap-2">
             <h1>Seja bem vindo a tela inicial</h1>
-            <span>aqui sera uma tela de login para entrar no prpjeto</span>
+            <span>aqui será uma tela de login para entrar no projeto</span>
             <PrimaryButton
                 actionText="Ir para o dashboard"
                 redirectRoute="/dashboard"
